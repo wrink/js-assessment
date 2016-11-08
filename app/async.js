4a9b74d942b51375c254ffbf8157a296729c0896
David Wikler <david@Davids-MacBook-Pro.local>
2016-11-08 17:29:09 -0500

    did array, asynch, recursion, object, regex

--- app/async.js
+++ app/async.js
@@ -2,24 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-		this.then = function(callback) {
-			setTimeout(callback, 0, value)
-		}
-		return this
+		return new Promise(function(callback) {
+			res(value)
+		})
   },
 
   manipulateRemoteData: function(url) {
-		this.then = function (callback) {
+		return new Promise(function (callback) {
 			req = new XMLHttpRequest();
 
 			req.onload = function() {
 				callback(req.response.people.map((x) => x.name).sort())
-				//callback(JSON.parse(response.target.response).people.map((x)=>x.name))
 			};
 			req.open('GET', url)
 			req.responseType = 'json'
 			req.send()
-		}
-		return this
+		})
   }
 };
